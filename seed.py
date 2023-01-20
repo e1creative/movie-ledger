31112e8e9c521afb6a1cc5ec275af93161d00a3b
Jason Takahashi <jmtakahashi@gmail.com>
2023-01-20 00:18:55 -0800

    Update seed file for production.

--- seed.py
+++ seed.py
@@ -4,15 +4,16 @@
 from models import db, User, Movie
 from app import app
 
+db.drop_all()
 db.create_all()
 
 
 ##### USER #####
 
-u1 = User(username='test', password='password', email='test@test.com', img_url='https://source.unsplash.com/random/300×300')
-u2 = User(username='test2', password='password2', email='test@test.com', img_url='https://source.unsplash.com/random/300×300')
+# u1 = User(username='test', password='password', email='test@test.com', img_url='https://source.unsplash.com/random/300×300')
+# u2 = User(username='test2', password='password2', email='test@test.com', img_url='https://source.unsplash.com/random/300×300')
 
-db.session.add(u1)
-db.session.add(u2)
+# db.session.add(u1)
+# db.session.add(u2)
 
-db.session.commit()
+# db.session.commit()
